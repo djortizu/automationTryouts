0315a6210bc99b505604b688551d69cfe84b931e
djortizu <38997527+djortizu@users.noreply.github.com>
2018-05-05 00:17:21 -0400

    Uploading files for GBH testing

---
+++ GBHMain.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.PageObjects;
+using Selenio.Core.SUT;
+using Selenio.Extensions;
+using Selenio.Tests.MsTest.SUT;
+using static Selenio.Tests.MsTest.SUT.App;
+
+namespace Selenio.Tests.MsTest.PageObjects.GBH
+{
+    public class GBHMain : PageObject
+    {
+        [FindsBy(How = How.Id, Using = "")]
+        public IWebElement GBHLogo { get; set; }
+
+        [WaitForThisElement]
+        [FindsBy(How = How.Id, Using = "")]
+        public IWebElement Inicio { get; set; }
+
+        [FindsBy(How = How.Id, Using = "")]
+        public IWebElement Servicios { get; set; }
+
+        [FindsBy(How = How.Id, Using = "")]
+        public IWebElement Portafolio { get; set; }
+
+        [FindsBy(How = How.Id, Using = "")]
+        public IWebElement Conocenos { get; set; }
+
+        [FindsBy(How = How.Id, Using = "")]
+        public IWebElement Empleos { get; set; }
+
+        [FindsBy(How = How.Id, Using = "")]
+        public IWebElement Blog { get; set; }
+
+        public virtual GBHMain Open()
+        {
+            App.Reporter.TestStep = "Navegar hacia la pagina oficial de GBH.";
+            Driver.Url = "http://gbh.com.do/";
+            this.WaitForScreen();
+            return this;
+        }
+
+        public virtual GBHMain HoverOverMenuItem(IWebElement element)
+        {
+            Actions movement = new Actions(Driver);
+            movement.MoveToElement(Servicios).MoveToElement(element).Build();
+            return this;
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Selenio.Extensions;
+using Selenio.Tests.MsTest.SUT;
+using static Selenio.Tests.MsTest.SUT.App;
+
+namespace Selenio.Tests.MsTest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class UnitTest1 : MasterTestClass
+    { 
+    
+        [TestMethod]
+        public void TestMethod1()
+        {
+            RunTest(() =>
+            {
+                GBHHome.Open();
+
+                App.Reporter.TestStep = "Hacer click en la opcion 'Inicio'";
+                GBHHome.Inicio.Click();
+                GBHHome.WaitForScreen();
+
+                App.Reporter.TestStep = "Hacer mouse over para desplegar menu en la opcion 'Servicios'";
+                GBHHome.HoverOverMenuItem(GBHHome.Servicios).Servicios.Click();
+
+                App.Reporter.TestStep = "Hacer click en la opcion 'Portafolio'";
+                GBHHome.Portafolio.Click();
+                GBHHome.WaitForScreen(element: GBHHome.Portafolio);
+
+                App.Reporter.TestStep = "Hacer click en la opcion 'Conocenos'";
+                GBHHome.Conocenos.Click();
+                GBHHome.WaitForScreen(element: GBHHome.Conocenos);
+
+                App.Reporter.TestStep = "Hacer click en la opcion 'Empleos'";
+                GBHHome.Empleos.Click();
+                GBHHome.WaitForScreen(element: GBHHome.Empleos);
+
+                App.Reporter.TestStep = "Hacer click en la opcion 'Blog'";
+                GBHHome.Blog.Click();
+                GBHHome.WaitForScreen(element: GBHHome.Blog);
+            });
+        }
+    }
+}
